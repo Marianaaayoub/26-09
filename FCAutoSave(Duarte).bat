b4a7daad4842aec6d4dcf77bed56ef800dccea74
Mariana Ayoub <marianaadel0301@gmail.com>
2025-09-26 14:22:15 -0300

    first commit

---
+++ FCAutoSave(Duarte).bat
@@ -0,0 +1,10 @@
+start https://github.com/?locale=pt-br
+git init
+git status
+git config --global user.name "Mariana Ayoub"
+git config --global user.email marianaadel0301@gmail.com
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Marianaaayoub/26-09.git
+git push -u origin main
